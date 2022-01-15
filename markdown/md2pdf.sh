a741474e942af0a4eeba0ca43021f991970f7635
andrey <andrey.batalev@gmail.com>
2022-01-15 19:21:52 +0300

    bump version of pandoc from 2.14.1 to 2.17.0.1, alpine of 3.14.1 to 3.15.0

--- markdown/md2pdf.sh
+++ markdown/md2pdf.sh
@@ -1,10 +1,6 @@
 #!/bin/sh
 
 pandoc \
-    $1 -f markdown \
-    --template /usr/share/eisvogel/eisvogel.latex \
-    --pdf-engine=xelatex \
-    -o $2 \
     -V lang=ru-RU \
     -V 'mainfont:Exo 2 Light Condensed' \
     -V 'sansfont:Exo 2 Regular Condensed' \
@@ -12,4 +8,8 @@ pandoc \
     -V 'mathfont:texgyredejavu-math.otf' \
     -V 'fontsize: 14pt' \
     -V 'code-block-font-size: \footnotesize' \
-    -V 'listings-disable-line-numbers: true'
+    -V 'listings-disable-line-numbers: true' \
+    $1 -f markdown \
+    --template /usr/share/eisvogel/eisvogel.tex \
+    --pdf-engine=xelatex \
+    -o $2
