9a005a2d45c4342a6c0cfab8fe08ee6d4c851aed
andrey <andrey.batalev@gmail.com>
2025-05-06 18:40:49 +0300

    java: use smartdockerbuilder

--- java/mk.sh
+++ java/mk.sh
@@ -1,30 +1,43 @@
-#!/bin/bash
+#!/bin/sh
 
 GITHUB_PUSH=0
 CREATE_TAR=0
 
-if [ "${GITHUB_PUSH}" = "1" ]; then
-    echo $CR_PAT | docker login ghcr.io -u abatalev --password-stdin
+CDIR=$(pwd)
+
+if [ ! -f "../tools/smartdockerbuilder/sdb" ]; then
+    if [ ! -d "../tools" ]; then
+        mkdir ../tools
+    fi 
+    cd ../tools || exit
+    git clone https://github.com/abatalev/smart-docker-builder.git smartdockerbuilder 
+    cd smartdockerbuilder || exit
+    ./mk.sh
+    cd "${CDIR}" || exit
 fi
 
-function build_image () {
+if [ "${GITHUB_PUSH}" = "1" ]; then
+    echo "$CR_PAT" | docker login ghcr.io -u abatalev --password-stdin
+fi
+
+build_image () {
     echo "-[build ${3}]------------" 
-    docker build -t ${3} -f ${2} .
-    docker tag ${3} ghcr.io/${3}
+    docker build -t "${3}" -f "${2}" .
+    docker tag "${3}" "ghcr.io/${3}"
     if [ "${CREATE_TAR}" = "1" ]; then
         echo "-[save images]-----------"
-        docker save -o ../${1} ${3} ${4}
+        docker save -o "../${1}" "${3}" "${4}"
     fi
 
     if [ "$GITHUB_PUSH" = "1" ]; then
         echo "-[publish image ${1}]----"
-        docker push ghcr.io/${3}
+        docker push "ghcr.io/${3}"
     fi
     echo "-[build ${3} - done ]-------"
 }
 
-function test_image () {
-    cat Dockerfile.fontcheck | sed s%XXX%${1}%g > Dockerfile.test
+test_image () {
+    cat Dockerfile.fontcheck | sed "s%XXX%${1}%g" > Dockerfile.test
     docker build -t abatalev/fontcheck -f Dockerfile.test . 2> /dev/null
     rm Dockerfile.test
     docker run --rm abatalev/fontcheck 1> /dev/null 2> /dev/null
@@ -39,7 +52,7 @@ function test_image () {
 
 BUILD_OLD=0
 
-if [ "$BUILD_OLD" == "1" ]; then
+if [ "$BUILD_OLD" = "1" ]; then
 
     echo "### Tests java 8"
     
@@ -65,6 +78,7 @@ fi
 echo "### Tests java 17"
 echo " -- Latest GA release: 17.0.13 (17.0.13+7)"
 
+../tools/smartdockerbuilder/sdb Dockerfile.liberica17l
 build_image liberica17.tar Dockerfile.liberica17 abatalev/liberica:17.0.13-12-jre-alpine3.16-ttf bellsoft/liberica-openjre-alpine-musl:17.0.13-12
 #build_image lrc17.tar Dockerfile.lrc17 abatalev/lrc:17.0.13-12-jre-alpine3.16-ttf bellsoft/liberica-runtime-container:jre-17.0.13-musl
 
@@ -77,6 +91,7 @@ test_image abatalev/liberica:17.0.13-12-jre-alpine3.16-ttf
 echo "### Tests java 21"
 echo " -- Latest GA release: 21.0.5 (21.0.5+11)"
 
+../tools/smartdockerbuilder/sdb Dockerfile.liberica21
 build_image liberica21.tar Dockerfile.liberica21 abatalev/liberica:21.0.5-11-jre-alpine3.18-ttf bellsoft/liberica-openjre-alpine-musl:21.0.5-11
 
 test_image bellsoft/liberica-openjre-alpine-musl:21.0.5-11
